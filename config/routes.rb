1b40acedd89511230c29a579e93c06d5b3a325f8
YutoSuzu <areionkg7@gmail.com>
2022-11-19 08:57:56 +0000

    [ADD]ユーザーモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+  #管理者用
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  #ユーザー用
+  devise_for :users, controllers: {
+    registration: "public/registration",
+    sessions: "public/sessions"
+  }
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
